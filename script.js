978d0370e729742351309da14ae4f713a04e8f24
wasim-akram185 <mdwashimakram185@gmail.com>
2025-03-22 00:43:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields.");
+    } else {
+        alert("Message sent successfully!");
+        this.reset();
+    }
+});
